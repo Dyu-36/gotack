9a64249f900a350933fb7b55bd4851fe13c2b312
Dyu-36 <nguyenvanducduy2006@gmail.com>
2026-09-20 19:22:51 +0700

    refactor: drop legacy stream event subscriptions

--- internal/engine/link.go
+++ internal/engine/link.go
@@ -241,7 +241,7 @@ var (
 )
 
 var StreamKinds = []string{
-	"message", "session", "run_complete", "task_progress", "permission_request", "file",
+	"message", "session", "run_complete", "file",
 }
 
 type EventConsumer interface {
